1bd9f902ebc8268dd7c3e916e08ad2e504edae7a
crong12 <ongchinrong12@gmail.com>
2025-12-27 19:03:16 +0000

    fix csv import

--- apps/one_piece_dialogue_analysis.py
+++ apps/one_piece_dialogue_analysis.py
@@ -75,7 +75,7 @@ def _(mo, pd):
     # import dialogue dataset
     opDialogues = pd.read_csv(
         str(mo.notebook_location() / "public" / "one_piece_dialogues_emotions.csv"), 
-        compression={'method': None}
+        compression=None
     )
     return (opDialogues,)
 
